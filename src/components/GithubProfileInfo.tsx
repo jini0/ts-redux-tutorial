cc9e48909ed83c7098c00760399a546be66d5bea
jini <cathkid00@gmail.com>
2022-08-01 15:13:28 +0900

    지난주꺼 오류수정완

--- src/components/GithubProfileInfo.tsx
+++ src/components/GithubProfileInfo.tsx
@@ -9,6 +9,7 @@ type GithubProfileInfoProps = {
     bio: string;
     blog: string;
 }
+// 8.1 https://api.github.com/users/jini0 를 확인해보고 값이 null이 아닌걸 props에 넣어주고 뜨는지 확인해보자!!! 
 const GithubProfileInfo = ({ login, thumbnail, bio, blog }: GithubProfileInfoProps) => {
     return (
         <div>

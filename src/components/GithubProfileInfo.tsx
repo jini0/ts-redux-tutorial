a3bf9f1c175b7fbcb5e07ec98eac4bcfeece9d1d
jini <cathkid00@gmail.com>
2022-08-01 15:12:17 +0900

    지난주 금요일에 한거 오류수정

--- src/components/GithubProfileInfo.tsx
+++ src/components/GithubProfileInfo.tsx
@@ -3,17 +3,18 @@ import React from 'react';
 
 //props 타입지정
 type GithubProfileInfoProps = {
-    name: string;
+    // name: string;
+    login: string;
     thumbnail: string;
     bio: string;
     blog: string;
 }
-const GithubProfileInfo = ({ name, thumbnail, bio, blog }: GithubProfileInfoProps) => {
+const GithubProfileInfo = ({ login, thumbnail, bio, blog }: GithubProfileInfoProps) => {
     return (
         <div>
             <div>
                 <img src={thumbnail} alt="img" />
-                <div>{name}</div>
+                <div>{login}</div>
             </div>    
             <p>{bio}</p>
             <div>{blog !== '' && <a href={blog}>블로그</a>}</div>
--- src/containers/GithubProfileLoader.tsx
+++ src/containers/GithubProfileLoader.tsx
@@ -6,6 +6,8 @@ import GithubProfileInfo from '../components/GithubProfileInfo';
 import GithubUsernameForm from '../components/GithubUsernameForm';
 import { RootState } from '../modules';
 import { getUserProfileThunk } from '../modules/github';
+// 8.1 추가 - error 뜨던거 수정
+import type {} from 'redux-thunk/extend-redux';
 
 const GithubProfileLoader = () => {
     const { data, loading, error } = useSelector((state: RootState) => state.github.userProfile);
@@ -22,7 +24,9 @@ const GithubProfileLoader = () => {
             {error && <p>에러발생...</p>}
             {data && <GithubProfileInfo bio={data.bio}
             blog={data.blog}
-            name={data.name}
+            // name={data.name}     //name이 null값이라서 안나와서
+            // name={data.login}    //이렇게 name값에 login의 값을 넣어줘도 된다!! --> GithubProfileInfo.tsx에서 props에 name을 넣어주고 / props의 타입도 name의 타입을 지정해주면 된다!
+            login={data.login}
             thumbnail={data.avatar_url}
             />}   
         </>
